2145d28556af9d2f964073df388210ce27b19ec0
tarekhdeen <tarekhdeen@gmail.com>
2023-09-07 14:41:59 -0700

    2.4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 
-int main()
+/**
+ * Printig that exact string using 'puts'
+ */
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	return (0);
 }
