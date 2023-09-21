622f8019c843c6f7f7b69d330b6a26bb08c1672a
tarekhdeen <tarekhdeen@gmail.com>
2023-09-21 15:03:43 -0700

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,19 +9,22 @@
 char *rot13(char *str)
 {
 	char *ptr = str;
+	int i;
 
 	while (*ptr)
 	{
-		if ((*ptr >= 'a' && *ptr <= 'm') || (*ptr >= 'A' && *ptr <= 'M'))
+		for (i = 0; i < 2; i++)
 		{
-			*ptr += 13;
-		}
-		else if ((*ptr >= 'n' && *ptr <= 'z') || (*ptr >= 'N' && *ptr <= 'Z'))
-		{
-			*ptr -= 13;
+			if ((*ptr >= 'a' && *ptr <= 'm') || (*ptr >= 'A' && *ptr <= 'M'))
+			{
+				*ptr += 13;
+			}
+			else if ((*ptr >= 'n' && *ptr <= 'z') || (*ptr >= 'N' && *ptr <= 'Z'))
+			{
+				*ptr -= 13;
+			}
 		}
 		ptr++;
 	}
-
 	return (str);
 }
