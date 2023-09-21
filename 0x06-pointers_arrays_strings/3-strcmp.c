4e6c20548a3f946277f8dbce41f05e8c8fe3ceca
tarekhdeen <tarekhdeen@gmail.com>
2023-09-21 00:11:47 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,15 +13,15 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 < *s2)
-			return (-1);
+			return (-15);
 		if (*s1 > *s2)
-			return (1);
+			return (15);
 		s1++;
 		s2++;
 	}
 	if (*s1 == '\0' && *s2 == '\0')
 		return (0);
 	if (*s1 == '\0')
-		return (-1);
-	return (1);
+		return (-15);
+	return (15);
 }
