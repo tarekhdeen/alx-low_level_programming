99f2304edf917a909feff6fc0faa7a3fc93ef91b
tarekhdeen <tarekhdeen@gmail.com>
2023-09-25 14:20:21 -0700

    2,3.4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stddef.h>
+#include <string.h>
 
 /**
  * _strspn - a function that gets the length of a prefix substring.
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stddef.h>
+#include <string.h>
 
 /**
  * _strpbrk - a function that searches a string for any of a set of bytes.
