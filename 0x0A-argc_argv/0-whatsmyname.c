5de68eb3c1bed8e9efdf2da2972dc0b27d3a0001
tarekhdeen <tarekhdeen@gmail.com>
2023-10-02 14:25:23 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,19 +2,19 @@
 
 /**
  * main - Entry point
- * @argc:  stands for argument count.
- * @argv: stands for argument vector.
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	int i;
+	char *filename = __FILE__;
 
-	for (i = 0; argv[0][i] != '\0'; i++)
+	while (*filename != '\0')
 	{
-		_putchar(arg[0][i]);
+		_putchar(*filename);
+		filename++;
 	}
+
 	_putchar('\n');
 
 	return (0);
