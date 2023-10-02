7954a60773c1763d3b625e7603bf564dd0f18cdc
tarekhdeen <tarekhdeen@gmail.com>
2023-10-02 13:59:57 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc:  stands for argument count.
+ * @argv: stands for argument vector.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; argv[0][i] != '\0'; i++)
+	{
+		_putchar(arg[0][i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
