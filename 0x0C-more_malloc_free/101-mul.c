35c51888535c61a6357af3c58b95908a0284965f
tarekhdeen <tarekhdeen@gmail.com>
2023-10-08 12:16:20 -0700

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,14 +3,23 @@
 
 /**
  * main - multiplies two positive numbers.
+ * @argc: argc.
+ * @argv: argv.
  *
  * Return: Always 0
  */
-int main(void)
+int main(int argc, char *argv[])
 {
 	int num1, num2, mul;
 
-	scanf("%d %d", &num1, &num2);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
 	if (num1 < 0 || num2 < 0)
 	{
