df3f58d2db6cbc8b51712de2cc4f5a978b93f192
tarekhdeen <tarekhdeen@gmail.com>
2023-10-19 13:45:39 -0700

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,10 @@
 section .data
 hello_message db 'Hello, Holberton', 0
-format db '%s', 0
+format db '%s', 10, 0 ; Added newline character (10 in ASCII)
 
 section .text
 global main
-extern printf, exit
-
+extern printf
 
 main:
 ; Prepare arguments for printf
@@ -15,10 +14,7 @@ mov rsi, hello_message  ; Message address
 ; Call printf
 call printf
 
-; Exit program using syscall
-mov eax, 60          ; System call number for exit
-xor edi, edi         ; Exit code 0
-syscall             ; Make system call
-
-; This point is never reached, but it's good to have a return value for main
+; Return from main (this will implicitly exit the program)
+xor eax, eax
 ret
+
