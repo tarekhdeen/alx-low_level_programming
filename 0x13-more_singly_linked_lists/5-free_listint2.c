a3b2108867726a39d993bbbce5740e1049b4f2e8
tarekhdeen <tarekhdeen@gmail.com>
2023-10-24 04:09:38 -0700

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
-
-		*head = NULL;
 	}
+
+	 head = NULL;
 
 }
